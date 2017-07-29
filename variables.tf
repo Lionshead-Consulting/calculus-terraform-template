a6d4eff763a3b2bef0f5c4d9af61158fc6be8c06
Michael Iglesias <mike.iglesias11@gmail.com>
2017-07-29 11:23:58 -0400

    quick changes

--- variables.tf
+++ variables.tf
@@ -2,6 +2,10 @@ variable "region" {
   default = "us-east-1"
 }
 
+variable "role" {
+  description = "ARN of the IAM Role your Lambda function will assume."
+}
+
 variable "memory_size" {
   default = 128
 }
