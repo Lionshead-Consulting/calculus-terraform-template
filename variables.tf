581bf328874e69ce637897092bd95d948d41449b
Michael Iglesias <mike.iglesias11@gmail.com>
2017-08-20 19:29:13 -0400

    added s3 object upload

--- variables.tf
+++ variables.tf
@@ -6,6 +6,10 @@ variable "role" {
   description = "ARN of the IAM Role your Lambda function will assume."
 }
 
+variable "bucket" {
+  description = "S3 Bucket where Lambda zip should be uploaded to."
+}
+
 variable "memory_size" {
   default = 128
 }
